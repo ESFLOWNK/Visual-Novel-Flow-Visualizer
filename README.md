251268ace1b5e841d29b42b8387b92e88497ab61
ESFLOWNK <otakuflownk@gmail.com>
2025-06-28 22:57:31 -0400

    Update README and add the plus button image

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 VNFV (Visual Novel Flow Visualizer) is a program designed to planificate a visual novel's history flow.
 
+# How to compile
+
+> This program was made in Linux, so it may not work on other operative systems.
+
+Install all the libraries and software listed in [requirements](#requirements).
+
+The proceed to run the "Makefile" file.
+
+```Shell
+make
+```
+
+And finally run the program.
+
+```Shell
+./vnfv
+```
+
 # Requirements
 
 - SDL2 library.
--- src/drawgui.c
+++ src/drawgui.c
@@ -12,11 +12,12 @@ void drawInterface(SDL_Renderer *renderer, SDL_Window *screen) {
     menu.w = 3;
     menu.h = windowHeight;
     menu.x = getPercentage(MENU_HSIZE_PERCENTAGE, windowWidth) - menu.w / 2;
+    menu.y = 0;
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderFillRect(renderer, &menu);
 
-/*     SDL_Texture *plusbuttonimg;
+    SDL_Texture *plusbuttonimg;
     SDL_Rect plusbuttonrect;
     plusbuttonimg = IMG_LoadTexture(renderer, "files/images/plusbutton.png");
     plusbuttonrect.w = 30;
@@ -24,7 +25,7 @@ void drawInterface(SDL_Renderer *renderer, SDL_Window *screen) {
     plusbuttonrect.x = getPercentage(PLUS_BUTTON_XPOS_PERCENTAGE, windowWidth) - plusbuttonrect.w / 2;
     plusbuttonrect.y = getPercentage(PLUS_BUTTON_YPOS_PERCENTAGE, windowHeight) - plusbuttonrect.h / 2;
 
-    SDL_RenderCopy(renderer, plusbuttonimg, NULL, &plusbuttonrect); */
+    SDL_RenderCopy(renderer, plusbuttonimg, NULL, &plusbuttonrect);
 }
 
 void startDrawing(SDL_Renderer *renderer, SDL_Window *screen) {
