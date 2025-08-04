e105850ebc8c4f290ee2e35534f1fc7427cebfd5
ESFLOWNK <otakuflownk@gmail.com>
2025-08-03 23:11:53 -0400

    Add utils.c file

--- src/utils.c
+++ src/utils.c
@@ -3,3 +3,12 @@
 int getPercentage(int percentage, int of) {
     return ((float) of / 100) * percentage;
 }
+
+unsigned char isInside(int ax, int ay, int bx, int by, int bw, int bh) {
+    // It checks if a is inside b
+    if(ax > bx && ax < bx + bw && ay > by && by < by + bh) {
+        return 1;
+    }
+
+    return 0;
+}
